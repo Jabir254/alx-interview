a050aa82e1d81161bea6fd7285064ff5c8bf14d3
jabir254 <jabirkullow@gmail.com>
2023-11-09 12:50:07 +0300

    fixed all

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,37 +1,37 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
-function getMovieCharacters(movieId) {
-	const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+function getMovieCharacters (movieId) {
+  const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-	request(filmUrl, (error, response, body) => {
-		if (!error && response.statusCode === 200) {
-			const filmData = JSON.parse(body);
-			const charactersUrls = filmData.characters;
+  request(filmUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const filmData = JSON.parse(body);
+      const charactersUrls = filmData.characters;
 
-			charactersUrls.forEach((characterUrl) => {
-				request(characterUrl, (error, response, body) => {
-					if (!error && response.statusCode === 200) {
-						const characterData = JSON.parse(body);
-						console.log(characterData.name);
-					} else {
-						console.error(
+      charactersUrls.forEach((characterUrl) => {
+        request(characterUrl, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            console.log(characterData.name);
+          } else {
+            console.error(
 							`Failed to fetch character data for ${characterUrl}`
-						);
-					}
-				});
-			});
-		} else {
-			console.error(`Failed to fetch movie data for ID: ${movieId}`);
-		}
-	});
+            );
+          }
+        });
+      });
+    } else {
+      console.error(`Failed to fetch movie data for ID: ${movieId}`);
+    }
+  });
 }
 
 // Usage: Pass the movie ID as an argument
 const movieId = process.argv[2];
 if (!movieId) {
-	console.log("Please provide the Movie ID as a command-line argument.");
+  console.log('Please provide the Movie ID as a command-line argument.');
 } else {
-	getMovieCharacters(movieId);
+  getMovieCharacters(movieId);
 }
